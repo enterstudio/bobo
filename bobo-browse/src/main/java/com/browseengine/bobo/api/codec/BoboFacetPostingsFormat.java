d6f47f4a5276a85d588c7c734c9f5ced15aebab0
John Wang <john.wang@gmail.com>
2013-09-22 16:24:31 -0700

    added posting format handler

--- bobo-browse/src/main/java/com/browseengine/bobo/api/codec/BoboFacetPostingsFormat.java
+++ bobo-browse/src/main/java/com/browseengine/bobo/api/codec/BoboFacetPostingsFormat.java
@@ -2,36 +2,71 @@ package com.browseengine.bobo.api.codec;
 
 import java.io.IOException;
 
+import org.apache.lucene.codecs.BlockTreeTermsReader;
+import org.apache.lucene.codecs.BlockTreeTermsWriter;
 import org.apache.lucene.codecs.FieldsConsumer;
 import org.apache.lucene.codecs.FieldsProducer;
 import org.apache.lucene.codecs.PostingsFormat;
-import org.apache.lucene.codecs.lucene41.Lucene41PostingsReader;
-import org.apache.lucene.codecs.lucene41.Lucene41PostingsWriter;
+import org.apache.lucene.codecs.PostingsReaderBase;
+import org.apache.lucene.codecs.PostingsWriterBase;
 import org.apache.lucene.index.SegmentReadState;
 import org.apache.lucene.index.SegmentWriteState;
+import org.apache.lucene.util.IOUtils;
 
 public class BoboFacetPostingsFormat extends PostingsFormat {
 
   private static final String NAME = "BoboPostingsFormat";
-  
+  private final DocIdSetCollector docIdSetCollector;
   public BoboFacetPostingsFormat() {
     super(NAME);
+    docIdSetCollector = getPostingsDocCollector();
   }
-
+  
+  public static final String DOC_EXTENSION = "doc";
+  
+  protected DocIdSetCollector getPostingsDocCollector() {
+    return new OpenBitsetDocIdSetCollector();
+  }
+  
   @Override
   public FieldsConsumer fieldsConsumer(SegmentWriteState state)
       throws IOException {
-    Lucene41PostingsWriter postingsWriter = new Lucene41PostingsWriter(state);
-    return new BoboFacetFieldsConsumer(state, postingsWriter);
-  }
+    PostingsWriterBase docsWriter = null;
 
+    boolean success = false;
+    try {
+      docsWriter = new BoboFacetPostingsWriter(state, docIdSetCollector);
+      FieldsConsumer ret = new BlockTreeTermsWriter(state, docsWriter, BlockTreeTermsWriter.DEFAULT_MIN_BLOCK_SIZE, BlockTreeTermsWriter.DEFAULT_MAX_BLOCK_SIZE);
+      success = true;
+      return ret;
+    } finally {
+      if (!success) {
+        IOUtils.closeWhileHandlingException(docsWriter);
+      }
+    }
+  }
+  
   @Override
   public FieldsProducer fieldsProducer(SegmentReadState state)
       throws IOException {
-    Lucene41PostingsReader postingsReader = new Lucene41PostingsReader(state.directory, 
-        state.fieldInfos, state.segmentInfo, state.context, state.segmentSuffix);
-    
-    return new BoboFacetFieldsProducer(state, postingsReader);
-  }
+    PostingsReaderBase docsReader = null;
 
+    boolean success = false;
+    try {
+      docsReader = new BoboFacetPostingsReader(state, docIdSetCollector);
+      FieldsProducer ret = new BlockTreeTermsReader(
+                                                    state.directory, state.fieldInfos, state.segmentInfo,
+                                                    docsReader,
+                                                    state.context,
+                                                    state.segmentSuffix,
+                                                    state.termsIndexDivisor);
+      success = true;
+      return ret;
+    } finally {
+      if (!success) {
+        IOUtils.closeWhileHandlingException(docsReader);
+      }
+    }
+  }
+  
 }
